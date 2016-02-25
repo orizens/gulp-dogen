cd8e4a4a1d3203212ce17a8f9205673d7ff0bec2
orizens <farhioren@gmail.com>
2016-02-25 21:51:12 +0200

    fixed: undefined when using camelCase

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function listGeneratorsToConsole () {
 }
 
 function toCamelCase (selector) {
-    var converted = selector
+    return selector
         .replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); })
         .replace(/^[a-z]/g, function (g) { return g.toUpperCase(); })
         .replace(/\[|]/g, "");
